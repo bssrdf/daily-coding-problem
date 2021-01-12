fea8c63d3a07a283d56fa303dd92c902a82e1850
bssrdf <merlintiger@hotmail.com>
2021-01-12 17:53:52 -0500

    corrected the solution to satisfy the problem specification

--- solutions/problem_235.py
+++ solutions/problem_235.py
@@ -1,13 +1,23 @@
 def find_min_max(nums):
-    mini = maxi = nums[0]
-    for num in nums[1:]:
-        if num < mini:
-            mini = num
-            continue
-        elif num > maxi:
-            maxi = num
-
-    return mini, maxi
+    n = len(nums)
+    if n%2 == 0:
+        if nums[0] > nums[1]:
+            mini, maxi = nums[1], nums[0]
+        else:
+            mini, maxi = nums[0], nums[1]
+        i = 2
+    else:
+        mini = maxi = nums[0]
+        i = 1
+    while i < n-1:
+        if nums[i] > nums[i+1]:
+            if nums[i]   > maxi: maxi = nums[i]
+            if nums[i+1] < mini: mini = nums[i+1]
+        else:
+            if nums[i+1] > maxi: maxi = nums[i+1]
+            if nums[i]   < mini: mini = nums[i]
+        i += 2
+    return mini, maxi    
 
 
 # Tests
